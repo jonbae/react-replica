a86b7c250ae26e24c285205266ce209ad441fab0
jonbae <jonbae61596@gmail.com>
2020-06-02 22:36:21 -0400

    add eventListeners and add/update/delete nodes

--- index.js
+++ index.js
@@ -26,20 +26,62 @@ const createDom = (fiber) => {
   const dom =
     fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
-      : document.createElement(element.type);
-  const isProperty = (key) => key !== "children";
-  Object.keys(fiber.props)
-    .filter(isProperty)
-    .forEach((name) => {
-      dom[name] = fiber.props[name];
-    });
+      : document.createElement(fiber.type);
+
+  updateDom(dom, {}, fiber.props);
 
   return dom;
+};
+const isEvent = (key) => key.startsWith("on");
+const isProperty = (key) =>
+  key !== "children" && !isEvent(key);
+const isNew = (prev, next) => (key) =>
+  prev[key] !== next[key];
+const isGone = (prev, next) => (key) => !(key in next);
+
+const updateDom = (dom, prevProps, nextProps) => {
+  //TODO
+
+  //Remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter(
+      (key) =>
+        !(key in nextProps) ||
+        isNew(prevProps, nextProps)(key)
+    )
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[name]);
+    });
+
+  // Remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach((name) => (dom[name] = ""));
+
+  // Set new or changed properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => (dom[name] = nextProps[name]));
+
+  // Add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, nextProps[name]);
+    });
 };
 
 const commitRoot = () => {
   //TODO add nodes to dom
+  deletions.forEach(commitWork);
   commitWork(wipRoot.child);
+  currentRoot = wipRoot;
   wipRoot = null;
 };
 
@@ -49,7 +91,23 @@ const commitWork = (fiber) => {
   }
 
   const domParent = fiber.parent.dom;
-  domParent.appendChild(fiber.dom);
+  if (
+    fiber.effectTag === "PLACEMENT" &&
+    fiber.dom !== null
+  ) {
+    domParent.appendChild(fiber.dom);
+  } else if (fiber.effectTag === "DELETION") {
+    domParent.removeChild(fiber.dom);
+  } else if (
+    fiber.effectTag === "UPDATE" &&
+    fiber.dom !== null
+  ) {
+    updateDom(
+      fiber.dom,
+      fiber.alternate.props,
+      fiber.props
+    );
+  }
   commitWork(fiber.child);
   commitWork(fiber.sibling);
 };
@@ -61,18 +119,16 @@ const render = (element, container) => {
     props: {
       children: [element],
     },
+    alternate: currentRoot,
   };
-
+  deletions = [];
   nextUnitOfWork = wipRoot;
-
-  //   element.props.children.forEach((child) =>
-  //     render(child, dom)
-  //   );
-  //   container.appendChild(dom);
 };
 
 let nextUnitOfWork = null;
+let currentRoot = null;
 let wipRoot = null;
+let deletions = null;
 
 const workLoop = (deadline) => {
   let shouldYield = false;
@@ -95,38 +151,17 @@ const performUnitOfWork = (fiber) => {
   if (!fiber.dom) {
     fiber.dom = createDom(fiber);
   }
-  if (fiber.parent) {
-    fiber.parent.dom.appendChild(fiber.dom);
-  }
-  //TODO create new fibers
+  // delete?
+  //   if (fiber.parent) {
+  //     fiber.parent.dom.appendChild(fiber.dom);
+  //   }
+
   const elements = fiber.props.children;
-  let index = 0;
-  let prevSibling = null;
-
-  while (index < elements.length) {
-    const element = elements[index];
-
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null,
-    };
-
-    if (index === 0) {
-      fiber.child = newFiber;
-    } else {
-      prevSibling.sibling = newFiber;
-    }
-
-    prevSibling = newFiber;
-    index++;
-  }
-
-  //TODO return next unit of work
+  reconcileChildren(fiber, elements);
   if (fiber.child) {
     return fiber.child;
   }
+
   let nextFiber = fiber;
   while (nextFiber) {
     if (nextFiber.sibling) {
@@ -135,6 +170,65 @@ const performUnitOfWork = (fiber) => {
     nextFiber = nextFiber.parent;
   }
 };
+//TODO create new fibers
+function reconcileChildren(wipFiber, elements) {
+  let index = 0;
+  let oldFiber =
+    wipFiber.alternate && wipFiber.alternate.child;
+  let prevSibling = null;
+
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index];
+
+    const newFiber = null;
+
+    //TODO compare oldFiber to element
+    const sameType =
+      oldFiber && element && element.type == oldFiber.type;
+
+    if (sameType) {
+      //TODO update the node
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: "UPDATE",
+      };
+    }
+    if (element && !sameType) {
+      //TODO add this node
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: "PLACEMENT",
+      };
+    }
+
+    if (oldFiber && !sameType) {
+      //TODO delete the oldFiber's node
+      oldFiber.effectTag = "DELETION";
+      deletions.push(oldFiber);
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
+
+    if (index === 0) {
+      wipFiber.child = newFiber;
+    } else if (element) {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+}
 
 const Pedantic = {
   createElement,
@@ -149,12 +243,21 @@ const Pedantic = {
 // )
 
 /** @jsx Pedantic.createElement */
-const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b />
-  </div>
-);
-
 const container = document.getElementById("root");
-Pedantic.render(element, container);
+
+const updateValue = (e) => {
+  rerender(e.target.value);
+};
+
+const rerender = (value) => {
+  const element = (
+    <div>
+      <input onInput={updateValue} value={value} />
+      <h2>Hello {value}</h2>
+    </div>
+  );
+
+  Pedantic.render(element, container);
+};
+
+rerender("World");
